1be7e430f87e8088df35d3a62ecf91973027e361
ishida_masakazu <u225210b@gmail.com>
2019-01-12 22:06:39 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'items#index'
+
+  devise_for :users
   resources :items
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
