449292ff8dd4e9ad386329e6cd85883408a39ba9
ishida_masakazu <u225210b@gmail.com>
2019-01-13 00:50:07 +0900

    memberネームスペースの作成とそれに伴うアクション/View/ルーティングの変更

---
+++ app/controllers/member/users_controller.rb
@@ -0,0 +1,36 @@
+class Member::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update]
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to [:member, @user], notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name)
+    end
+end
