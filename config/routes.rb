07a467f13d3b70fc73c99898f2873579c614c2cb
ishida_masakazu <u225210b@gmail.com>
2019-01-14 14:46:33 +0900

    member用のitems_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+
+  # Userログイン時
+  authenticated :user do
+    root 'member/items#index'
+  end
+
+  # User非ログイン時
   root 'items#index'
 
   devise_for :users
   namespace :member do
     resources :users, only: %i[show edit update]
+    resources :items
   end
-  resources :items
+  resources :items, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
