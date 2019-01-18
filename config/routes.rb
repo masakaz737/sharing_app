d7f3a06b2f9277c9ff77af5fe7d3f42bab23c3cd
ishida_masakazu <u225210b@gmail.com>
2019-01-18 23:23:45 +0900

    categoryのCLUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  
+
   root 'items#index'
 
   devise_for :users
+  resources :items, only: %i[index show]
+  resources :categories
+
   namespace :member do
     resources :users, only: %i[show edit update]
     resources :items
     resources :deals
   end
-  resources :items, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_115714) do
+ActiveRecord::Schema.define(version: 2019_01_18_134754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "deals", force: :cascade do |t|
     t.bigint "item_id", null: false
