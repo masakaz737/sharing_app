6ff6d4a6aeea89555e70957c8b433f167ec630f3
ishida_masakazu <u225210b@gmail.com>
2019-01-31 22:34:13 +0900

    N+1問題の解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.includes(:user, :categories)
   end
 
   # GET /items/1
