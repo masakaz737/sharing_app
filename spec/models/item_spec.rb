4a18544f916fcfdfb0f582a1febccf7911d2e21b
ishida_masakazu <u225210b@gmail.com>
2019-02-04 12:35:48 +0900

    itemモデルスペックについてcontext、itの記述を微修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe Item, type: :model do
 
     context 'アイテムのオーナーの場合' do
       let(:item){ create(:item, user: user1) }
-      it { expect(item.owner?(user1)).to be true }
+      it 'owner?の結果がtrueであること' do
+        expect(item.owner?(user1)).to be true
+      end
     end
 
     context 'アイテムのオーナーでない場合' do
       let(:item){ create(:item, user: user2) }
-      it { expect(item.owner?(user1)).to be false }
+      it 'owner?の結果がfalseであること' do
+        expect(item.owner?(user1)).to be false
+      end
     end
   end
 
@@ -21,16 +25,22 @@ RSpec.describe Item, type: :model do
 
     context '進行中の取引が存在する場合' do
       let!(:deal){ create(:deal, item: item, status: :delivered) }
-      it { expect(item.exist_progress_deals?).to be true }
+      it 'exist_progress_deals?の結果がtrueであること' do
+        expect(item.exist_progress_deals?).to be true
+      end
     end
 
-    context '進行中でない取引のみ存在する場合' do
+    context '進行中の取引きが存在しない場合' do
       let!(:deal){ create(:deal, item: item, status: :return) }
-      it { expect(item.exist_progress_deals?).to be false }
+      it 'exist_progress_deals?の結果がfalseであること' do
+        expect(item.exist_progress_deals?).to be false
+      end
     end
 
-    context 'アイテムに紐付く取引がない場合' do
-      it { expect(item.exist_progress_deals?).to be false }
+    context '取引が存在しない場合' do
+      it 'exist_progress_deals?の結果がfalseであること' do
+        expect(item.exist_progress_deals?).to be false
+      end
     end
   end
 end
