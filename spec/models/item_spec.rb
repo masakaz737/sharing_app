24fafb48c1cbc4845bdc67bef232b49d0ea16fdf
ishida_masakazu <u225210b@gmail.com>
2019-02-16 22:18:16 +0900

    self.search_byメソッドのテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -44,71 +44,55 @@ RSpec.describe Item, type: :model do
     end
   end
 
-  describe 'self.search(name, category_id)' do
+  describe 'self.search_by' do
     let(:category1){ create(:category, name: 'カテゴリ1')}
     let(:category2){ create(:category, name: 'カテゴリ2')}
-    let!(:item1){ create(:item, name: 'アイテムA', categories: [category1]) }
-    let!(:item2){ create(:item, name: 'アイテムA', categories: [category2]) }
-    let!(:item3){ create(:item, name: 'アイテムB', categories: [category1]) }
-    let!(:item4){ create(:item, name: 'ITEM', categories: [category2]) }
 
-    context '名前を空欄、カテゴリ「指定なし」で検索した場合' do
-      let(:search_item_without_name_and_category){ Item.search('', '') }
+    context '検索条件が存在しない場合' do
+      before do
+        create(:item)
+      end
 
-      it 'self.searchの戻り値にitem1,item2,item3,item4が全て含まれること' do
-        expect(search_item_without_name_and_category).to include(item1, item2, item3, item4)
+      it 'self.search_byの戻り値が1つであること' do
+        expect(Item.search_by('', '').count).to eq 1
       end
     end
 
-    context '名前「アイテムA」、カテゴリ「カテゴリ1」で検索した場合' do
-      let(:search_item_with_name_and_category){ Item.search('アイテムA', category1.id) }
+    context '検索条件が存在する場合' do
+      context 'name検索' do
+        before do
+          create(:item, name: 'アイテム1')
+          create(:item, name: 'アイテム')
+          create(:item, name: 'ITEM')
+        end
 
-      it 'self.searchの戻り値にitem1のみが含まれること' do
-        expect(search_item_with_name_and_category).to include(item1)
-        expect(search_item_with_name_and_category).not_to include(item2, item3, item4)
+        it 'self.search_byの戻り値が2つであること' do
+          expect(Item.search_by('アイテム', '').count).to eq 2
+        end
       end
-    end
 
-    context '名前「アイテムA」、カテゴリ「指定なし」で検索した場合' do
-      let(:search_item_without_category){ Item.search('アイテムA', '') }
+      context 'category検索' do
+        before do
+          create(:item, categories: [category1])
+          create(:item, categories: [category2])
+        end
 
-      it 'self.searchの戻り値にitem1,item2のみが含まれること' do
-        expect(search_item_without_category).to include(item1, item2)
-        expect(search_item_without_category).not_to include(item3, item4)
+        it 'self.search_byの戻り値が1つであること' do
+          expect(Item.search_by('', category1.id).count).to eq 1
+        end
       end
-    end
 
-    context '名前を空欄、カテゴリ「カテゴリ1」で検索した場合' do
-      let(:search_item_without_name){ Item.search('', category1.id) }
+      context '複合検索' do
+        before do
+          create(:item, name: 'アイテム1', categories: [category1])
+          create(:item, name: 'アイテム1', categories: [category2])
+          create(:item, name: 'アイテム2', categories: [category1])
+          create(:item, name: 'アイテム2', categories: [category2])
+        end
 
-      it 'self.searchの戻り値にitem1,item3のみが含まれること' do
-        expect(search_item_without_name).to include(item1, item3)
-        expect(search_item_without_name).not_to include(item2, item4)
-      end
-    end
-
-    context '名前「アイテム」、カテゴリ「指定なし」で検索した場合' do
-      let(:search_item_with_name_kana){ Item.search('アイテム', '') }
-
-      it 'self.searchの戻り値にitem1,item2,item3のみが含まれること' do
-        expect(search_item_with_name_kana).to include(item1, item2, item3)
-        expect(search_item_with_name_kana).not_to include(item4)
-      end
-    end
-
-    context '名前「A」、カテゴリ「指定なし」で検索した場合' do
-      let(:search_item_with_name_a){ Item.search('A', '') }
-
-      it 'self.searchの戻り値が空であること' do
-        expect(search_item_with_name_a).not_to include [item1, item2, item3, item4]
-      end
-    end
-
-    context '存在しない名前「アイテムC」、カテゴリ「指定なし」で検索した場合' do
-      let(:search_item_with_name_c){ Item.search('アイテムC', '') }
-
-      it 'self.searchの戻り値が空であること' do
-        expect(search_item_with_name_c).not_to include [item1, item2, item3, item4]
+        it 'self.search_byの戻り値が1つであること' do
+          expect(Item.search_by('アイテム1', category1.id).count).to eq 1
+        end
       end
     end
   end
