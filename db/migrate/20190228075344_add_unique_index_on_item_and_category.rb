3ce2f2ef60c5eafddeeeda185a4a35209969fa9f
ishida_masakazu <u225210b@gmail.com>
2019-02-28 17:47:43 +0900

    migrationファイル名修正

--- db/migrate/20190228075344_add_unique_index_on_item_and_category.rb
+++ db/migrate/20190228075344_add_unique_index_on_item_and_category.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexToItemAndCategory < ActiveRecord::Migration[5.2]
+class AddUniqueIndexOnItemAndCategory < ActiveRecord::Migration[5.2]
   def change
     add_index :item_categories, [:item_id, :category_id], unique: true
   end
