ab60aeef99fce721f4a8416ebb873427dbf31155
ishida_masakazu <u225210b@gmail.com>
2019-03-03 12:06:16 +0900

    ItemCategoryのpresence: trueバリデーションを削除

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -2,6 +2,5 @@ class ItemCategory < ApplicationRecord
   belongs_to :item
   belongs_to :category
 
-  validates :item_id, presence: true
-  validates :category_id, presence: true, uniqueness: { scope: :item_id }
+  validates :category_id, uniqueness: { scope: :item_id }
 end
